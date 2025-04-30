965147079e87f8241bef836f34ce7c03174d4757
unknown <rafif.purnomo@gmail.com>
2025-04-30 16:10:05 +0700

    refactoring adminController and multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -29,12 +29,3 @@ const upload = multer({
 });
 
 module.exports = upload;
-
-// const multer = require("multer");
-
-// // Konfigurasi multer untuk menyimpan file sebagai buffer di memori
-// const storage = multer.memoryStorage();
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
